d91fba65cc5e64dbef3bad632e7022d50ef7e642
Xsinser <xsinser175@gmail.com>
2024-08-18 16:42:22 +0300

    first

---
+++ DemoCharts/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace DemoCharts.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ DemoCharts/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DemoCharts.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
